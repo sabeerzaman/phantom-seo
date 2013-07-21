65c0e8cfb6ccb3850e4b38385770e36cbd5ff478
Sabeer Zaman <sabeer.zaman@gmail.com>
2013-07-21 01:16:23 -0400

    Refactor phantom-server.js to allow for a debug mode flag

--- phantom-server.js
+++ phantom-server.js
@@ -1,30 +1,37 @@
-// From: http://backbonetutorials.com/seo-for-single-page-apps/
+// Based on phantom-server.js from: 
+//	http://backbonetutorials.com/seo-for-single-page-apps/
 
 var JS_LATENCY_TIME_LIMIT = 300,
-	TOTAL_TIME_LIMIT = 30000;
+	TOTAL_TIME_LIMIT = 60000;
 
-var page = require('webpage').create();
-var system = require('system');
-var lastReceived = new Date().getTime();
-var beforeLastReceived = lastReceived;
-var requestCount = 0;
-var responseCount = 0;
-var requestIds = [];
-var startTime = new Date().getTime();
+var page = require('webpage').create(),
+	system = require('system'),
+	lastReceived = new Date().getTime(),
+	beforeLastReceived = lastReceived,
+	requestCount = 0,
+	responseCount = 0,
+	requestIds = [],
+	startTime = new Date().getTime(),
+	DEBUG_MODE = system.args[2] === '-debug';
 
 page.onResourceReceived = function(response) {
-	// console.log( 'REC: ' + response.url );
+	if ( DEBUG_MODE )
+		console.log( 'REC: ' + response.url );
 	if (requestIds.indexOf(response.id) !== -1) {
-		// beforeLastReceived = lastReceived;
-		lastReceived = new Date().getTime();
-		// var duration = lastReceived - beforeLastReceived;
-		// console.log( 'Since last request: ' + duration + ' ms' );
+		if ( DEBUG_MODE ) {
+			beforeLastReceived = lastReceived;
+			lastReceived = new Date().getTime();
+			var duration = lastReceived - beforeLastReceived;
+			console.log( 'Since last request: ' + duration + ' ms' );
+		}
 		responseCount++;
 		requestIds[requestIds.indexOf(response.id)] = null;
 	}
 };
+
 page.onResourceRequested = function(request) {
-	// console.log( 'REQ: ' + request.url );
+	if ( DEBUG_MODE )
+		console.log( 'REQ: ' + request.url );
 	if (requestIds.indexOf(request.id) === -1) {
 		requestIds.push(request.id);
 		requestCount++;
@@ -38,11 +45,15 @@ var checkComplete = function() {
 	// don't return until all requests are finished 
 	if ((new Date().getTime() - lastReceived > JS_LATENCY_TIME_LIMIT && requestCount === responseCount) || new Date().getTime() - startTime > TOTAL_TIME_LIMIT) {
 		clearInterval(checkCompleteInterval);
-		var duration = new Date().getTime() - startTime;
 		console.log(page.content);
-		// console.log( 'TOTAL TIME: ' + duration + ' ms' );
+		if ( DEBUG_MODE ) {
+			var duration = new Date().getTime() - startTime;
+			page.render( 'ss-' + startTime + '.png' );
+			console.log( 'Snapshot saved in: ss-' + startTime + '.png' );
+			console.log( 'TOTAL TIME: ' + duration + ' ms' );
+		}
 		phantom.exit();
 	}
-}
+};
 // Let us check to see if the page is finished rendering 
 var checkCompleteInterval = setInterval(checkComplete, 1);
