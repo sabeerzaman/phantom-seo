e7c1d18b7b7c0b8202d7717c429ddf66371f12e9
unknown <szaman@SZAMAN.desire2learn.d2l>
2013-07-25 15:56:03 -0400

    Make node server save rendered files, and simply resend pre-rendered file if it exists.

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
 // Express is our web server that can handle request
-var express = require('express');
-var app = express();
+var express = require('express'),
+	app = express(),
+	fs = require( 'fs' ),
+	htmlLocation = 'html_snapshots';
 
 var getContent = function(url, callback) {
 	var content = '';
-	// Here we spawn a phantom.js process, the first element of the
-	// array is our phantomjs script and the second element is our url
 	var phantom = require('child_process').spawn('phantomjs', ['phantom-server.js', url]);
 	phantom.stdout.setEncoding('utf8');
 	// Our phantom.js script is simply logging the output and
@@ -16,26 +16,85 @@ var getContent = function(url, callback) {
 	phantom.on('exit', function(code) {
 		if (code !== 0) {
 			console.log('We have an error');
-		} else {
-			// once our phantom.js script exits, let's call out call back
-			// which outputs the contents to the page
+		}
+		else {
 			callback(content);
 		}
 	});
 };
 
+var saveToFile = function( fragment, content ) {
+	var path = createFilePath( fragment ),
+		pathParts = path.split( /[\\\/]/g ),
+		filename = encodeURIComponent( pathParts.pop() ).replace( /[^\w.]/g, '-' ),
+		dirPath = pathParts.join( '/' );
+
+	if ( !fs.existsSync( dirPath ) ) {
+		var dir = '';
+		for ( var i = 0; i < pathParts.length; i++ ) {
+			dir += pathParts[i] + '/';
+			if ( dir && !fs.existsSync( dir ) ) {
+				fs.mkdirSync( dir );
+			}
+		}
+	}
+
+	htmlFile = fs.createWriteStream( dirPath + '/' + filename );
+	htmlFile.on( 'error', function( err ) {
+		console.log( 'There was an error writing to the file: ' );
+		console.log( err );
+	});
+	htmlFile.end( content );
+};
+
+var fileExists = function( fragment ) {
+	return fs.existsSync( createFilePath( fragment ) );
+};
+
+var createFilePath = function( fragment ) {
+	return htmlLocation + '/' + ( fragment || 'index' ) + '.html';
+};
+
 var respond = function(req, res) {
-	var path = req.path + '#!' + decodeURIComponent( req.query.hashbang ? req.query.hashbang : '' );
+	var fragment = decodeURIComponent( req.query.hashbang ? req.query.hashbang : '' ),
+		path = req.path + '#!' + fragment;
 
 	// Because we use [P] in htaccess we have access to this header
 	var url = 'http://' + req.headers['x-forwarded-host'] + path;
 
 	console.log( 'URL = ' + url );
-	getContent(url, function(content) {
-		content = content.replace( /<script.*?<\/script>/gim, '' );
-		console.log( 'Received (after removing script tags): ' ); console.log( content );
-		res.send(content);
-	});
+
+	if ( fileExists( fragment ) ) {
+		console.log( 'File already pre-rendered - sending it now' );
+		fs.readFile( createFilePath( fragment ), { encoding: 'utf8' },
+			function( err, data ) {
+				if ( err ) {
+					console.log( 'Error reading file: ' );
+					console.log( err );
+				}
+				else {
+					console.log( 'File contents: ' );
+					console.log( data );
+					res.writeHead( 200, {
+						'Content-Length': data.length,
+						'Content-Type': 'text/html'
+					});
+					res.end( data );
+				}
+			}
+		);
+	}
+	else {
+		getContent(url, function(content) {
+			content = content.replace( /<script.*?<\/script>/gim, '' );
+
+			console.log( 'Received (after removing script tags): ' );
+			console.log( content );
+
+			saveToFile( fragment, content );
+			res.send(content);
+		});
+	}
 };
 app.get(/(.*)/, respond);
 app.listen(3000);
